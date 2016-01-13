50c899ac74ad11b8a93c2463a0f332ae56f35331
Dietmar Eggemann <dietmar.eggemann@arm.com>
2016-01-13 15:49:44 +0000

    ANDROID: sched: Do eas idle balance regardless of the rq avg idle value
    EAS relies on idle balance to migrate a misfit task towards a cpu with
    higher capacity.
    
    When such a cpu becomes idle, idle balance should happen even if the rq
    avg idle is smaller than the sched migration cost (default 500us).
    
    The rq avg idle is updated during the wakeup of a task in case the rq has
    a non-null idle_stamp. This value stays unchanged and valid until the next
    task wakes up on this cpu after an idle period.
    
    So rq avg idle could be smaller than sched migration cost preventing the
    idle balance from happening. In this case we would be at the mercy of
    wakeup, periodic or nohz-idle load balancing to put another task on this
    cpu.
    
    To break this dependency towards rq avg idle make EAS idle balance
    independent from this rq avg idle has to be larger than sched migration
    cost.
    
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Signed-off-by: Andres Oportus <andresoportus@google.com>
    (cherry picked from commit 785367fc844d1f0d09d512e9d7df6e32ee340808)
    (cherry picked from commit af34cbb7fe39974cc6393dac9aeba3b779fa00e4)
    (cherry picked from commit e9f936c516e0cd6d9ee6007eaa2884d66dd25b1f)
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11292,7 +11292,7 @@ static int idle_balance(struct rq *this_rq, struct rq_flags *rf)
 	 */
 	rq_unpin_lock(this_rq, rf);
 
-	if (!force_lb && (this_rq->avg_idle < sysctl_sched_migration_cost ||
+	if (!energy_aware() && !force_lb && (this_rq->avg_idle < sysctl_sched_migration_cost ||
 	    !READ_ONCE(this_rq->rd->overload))) {
 		rcu_read_lock();
 		sd = rcu_dereference_check_sched_domain(this_rq->sd);
