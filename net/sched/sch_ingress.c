9eb3a74e447528369fdfba97eed512b2d00dc385
Jiri Pirko <jiri@mellanox.com>
2017-10-13 14:00:57 +0200

    BACKPORT: net: sched: store Qdisc pointer in struct block
    Prepare for removal of tp->q and store Qdisc pointer in the block
    structure.
    
    Signed-off-by: Jiri Pirko <jiri@mellanox.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 69d78ef25c7b0058674145500efb12255738ba8a)
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    (cherry picked from commit fb14036036a8ea8ebb769b13081bc26d011c5996)
    
    Change-Id: I0832defbd21119d71bd6f7d1ccfa791622e3c807

--- net/sched/sch_ingress.c
+++ net/sched/sch_ingress.c
@@ -61,7 +61,7 @@ static int ingress_init(struct Qdisc *sch, struct nlattr *opt)
 
 	net_inc_ingress_queue();
 
-	err = tcf_block_get(&q->block, &dev->ingress_cl_list);
+	err = tcf_block_get(&q->block, &dev->ingress_cl_list, sch);
 	if (err)
 		return err;
 
@@ -154,14 +154,18 @@ static int clsact_init(struct Qdisc *sch, struct nlattr *opt)
 	struct net_device *dev = qdisc_dev(sch);
 	int err;
 
+<<<<<<< HEAD
 	net_inc_ingress_queue();
 	net_inc_egress_queue();
 
 	err = tcf_block_get(&q->ingress_block, &dev->ingress_cl_list);
+=======
+	err = tcf_block_get(&q->ingress_block, &dev->ingress_cl_list, sch);
+>>>>>>> 69d78ef25c7b (net: sched: store Qdisc pointer in struct block)
 	if (err)
 		return err;
 
-	err = tcf_block_get(&q->egress_block, &dev->egress_cl_list);
+	err = tcf_block_get(&q->egress_block, &dev->egress_cl_list, sch);
 	if (err)
 		return err;
 
