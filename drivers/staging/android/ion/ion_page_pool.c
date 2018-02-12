1f481f45f5a4c7b021e45fd8368b19b58dddd145
Yisheng Xie <xieyisheng1@huawei.com>
2018-02-12 18:43:08 +0800

    staging: android: ion: Nuke ion_page_pool_init
    ion_page_pool.c now is used to apply pool APIs for system heap, which do
    not need do any initial at device_initcall. Therefore ion_page_pool_init
    can be nuked.
    
    Signed-off-by: Yisheng Xie <xieyisheng1@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Panchajanya Sarkar <panchajanya@azure-dev.live>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    (cherry picked from commit d719725ef20c4866ce6bc4b4566d533b6e44b267)

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -15,7 +15,6 @@
  */
 
 #include <linux/list.h>
-#include <linux/init.h>
 #include <linux/slab.h>
 #include <linux/swap.h>
 #include <linux/sched/signal.h>
@@ -223,9 +222,3 @@ void ion_page_pool_destroy(struct ion_page_pool *pool)
 {
 	kfree(pool);
 }
-
-static int __init ion_page_pool_init(void)
-{
-	return 0;
-}
-device_initcall(ion_page_pool_init);
