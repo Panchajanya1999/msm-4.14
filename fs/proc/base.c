652c7e7361a9c18db1921c767285c854c79c2cf2
Alexey Dobriyan <adobriyan@gmail.com>
2018-04-10 16:32:05 -0700

    proc: do mmput ASAP for /proc/*/map_files
    mm_struct is not needed while printing as all the data was already
    extracted.
    
    Link: http://lkml.kernel.org/r/20180309223120.GC3843@avx2
    Signed-off-by: Alexey Dobriyan <adobriyan@gmail.com>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2390,6 +2390,7 @@ proc_map_files_readdir(struct file *file, struct dir_context *ctx)
 		}
 	}
 	up_read(&mm->mmap_sem);
+	mmput(mm);
 
 	for (i = 0; i < nr_files; i++) {
 		p = flex_array_get(fa, i);
@@ -2403,7 +2404,6 @@ proc_map_files_readdir(struct file *file, struct dir_context *ctx)
 	}
 	if (fa)
 		flex_array_free(fa);
-	mmput(mm);
 
 out_put_task:
 	put_task_struct(task);
