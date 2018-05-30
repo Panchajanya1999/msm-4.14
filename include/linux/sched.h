f840db1911cab4e40193762522e7647e6259a93d
Sultan Alsawaf <sultanxda@gmail.com>
2018-05-29 17:43:25 -0700

    kernel: Add API to mark IRQs and kthreads as performance critical
    On devices with a CPU that contains heterogeneous cores (e.g., big.LITTLE),
    it can be beneficial to place some performance-critical IRQs and kthreads
    onto the performance CPU cluster in order to improve performance.
    
    This commit adds the following APIs:
    -kthread_run_perf_critical() to create and start a perf-critical kthread
    -IRQF_PERF_CRITICAL to schedule an IRQ and any threads it may have onto
     performance CPUs
    -PF_PERF_CRITICAL to mark a process (mainly kthread) as performance
     critical (this is used by kthread_run_perf_critical())
    
    In order to accomodate this new API, the following changes are made:
    -Performance-critical IRQs have their affinities reaffined upon exit from
     suspend (since the affinities are broken when non-boot CPUs are disabled)
    -Performance-critical IRQs and their threads have their affinities reset
     upon entering suspend, so that upon immediate suspend exit (when only the
     boot CPU is online), interrupts can be processed and interrupt threads
     can be scheduled onto an online CPU (otherwise we'd hit a kernel BUG)
    -Global cpu_lp_mask and cpu_perf_mask symbols are added to provide
     a cpumask of low-power and performance CPUs, respectively. This is
     set to the big.LITTLE layout used on msm8998 by default.
    -do_set_cpus_allowed() and __set_cpus_allowed_ptr() are modified to enforce
     a performance-critical kthread's affinity
    -Mark perf-critical IRQs with IRQD_AFFINITY_MANAGED so userspace can't mess
     with their affinity
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1558,6 +1558,7 @@ extern struct pid *cad_pid;
 #define PF_MUTEX_TESTER		0x20000000	/* Thread belongs to the rt mutex tester */
 #define PF_FREEZER_SKIP		0x40000000	/* Freezer should not count it as freezable */
 #define PF_SUSPEND_TASK		0x80000000      /* This thread called freeze_processes() and should not be frozen */
+#define PF_PERF_CRITICAL 0x01000000	/* Thread is performance-critical */
 
 /*
  * Only the _current_ task can read/write to tsk->flags, but other
--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1064,10 +1064,21 @@ void set_cpus_allowed_common(struct task_struct *p, const struct cpumask *new_ma
 	p->nr_cpus_allowed = cpumask_weight(new_mask);
 }
 
+static const struct cpumask *get_adjusted_cpumask(const struct task_struct *p,
+	const struct cpumask *req_mask)
+{
+	/* Force all performance-critical kthreads onto the big cluster */
+	if (p->flags & PF_PERF_CRITICAL)
+		return cpu_perf_mask;
+
+	return req_mask;
+}
 void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 {
 	struct rq *rq = task_rq(p);
 	bool queued, running;
+
+	new_mask = get_adjusted_cpumask(p, new_mask);
 
 	lockdep_assert_held(&p->pi_lock);
 
@@ -1120,6 +1131,7 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 		 * Kernel threads are allowed on online && !active CPUs
 		 */
 		cpu_valid_mask = cpu_online_mask;
+		new_mask = get_adjusted_cpumask(p, new_mask);
 	}
 
 	/*
