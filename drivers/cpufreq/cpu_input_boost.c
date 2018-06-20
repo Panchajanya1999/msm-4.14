3f9b5510b2125a3b79fb90bc1f9a618ee3c9e36f
Nathan Chancellor <natechancellor@gmail.com>
2018-06-19 19:56:57 -0700

    cpu_input_boost: Add ability to choose minimum frequency for big cluster
    Also, make sure we choose the actual minimum frequency if the configs
    are at their default value of zero using the maximum of the two.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: crian <crian84@gmail.com>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -50,6 +50,14 @@ static u32 get_boost_freq(struct boost_drv *b, u32 cpu)
 	return input_boost_freq_hp;
 }
 
+static u32 get_min_freq(struct boost_drv *b, u32 cpu)
+{
+	if (cpumask_test_cpu(cpu, cpu_lp_mask))
+		return CONFIG_REMOVE_INPUT_BOOST_FREQ_LP;
+
+	return CONFIG_REMOVE_INPUT_BOOST_FREQ_PERF;
+}
+
 static u32 get_boost_state(struct boost_drv *b)
 {
 	return atomic_read(&b->state);
@@ -180,7 +188,7 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 {
 	struct boost_drv *b = container_of(nb, typeof(*b), cpu_notif);
 	struct cpufreq_policy *policy = data;
-	u32 boost_freq, state;
+	u32 boost_freq, min_freq, state;
 
 	if (action != CPUFREQ_ADJUST)
 		return NOTIFY_OK;
@@ -207,7 +215,8 @@ static int cpu_notifier_cb(struct notifier_block *nb,
 		boost_freq = get_boost_freq(b, policy->cpu);
 		policy->min = min(policy->max, boost_freq);
 	} else {
-		policy->min = CONFIG_REMOVE_INPUT_BOOST_FREQ_LP;
+		min_freq = get_min_freq(b, policy->cpu);
+		policy->min = max(policy->cpuinfo.min_freq, min_freq);
 	}
 
 	return NOTIFY_OK;
