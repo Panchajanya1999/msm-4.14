3299d14497004ca46ae7aeb85ac6b87a041260f2
Francisco Franco <franciscofranco.1990@gmail.com>
2018-06-21 12:19:44 +0000

    drm: msm: sde: Boost CPU and DDR when a new frame is ready to be committed
    Seems like this is the correct place for this. crtc_commit goes high
    in CPU during UiBench invalidade test running top:
    
    CPU usage: 6.0% [crtc_commit:111]
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Atman Shah <masteratman@gmail.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -20,6 +20,8 @@
 #include <linux/sort.h>
 #include <linux/debugfs.h>
 #include <linux/ktime.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 #include <uapi/drm/sde_drm.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_crtc.h>
@@ -4206,6 +4208,9 @@ void sde_crtc_commit_kickoff(struct drm_crtc *crtc,
 	is_error = _sde_crtc_prepare_for_kickoff_rot(dev, crtc);
 
 	idle_pc_state = sde_crtc_get_property(cstate, CRTC_PROP_IDLE_PC_STATE);
+
+	devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+	cpu_input_boost_kick();
 
 	list_for_each_entry(encoder, &dev->mode_config.encoder_list, head) {
 		struct sde_encoder_kickoff_params params = { 0 };
