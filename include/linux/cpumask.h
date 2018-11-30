8d22379ff6a37edc7c9cbc6faf104dd6d2d62ea9
Sultan Alsawaf <sultan@kerneltoast.com>
2018-11-30 02:37:09 -0800

    cpumask: Add cpumasks for big and little CPU clusters
    Add cpu_lp_mask and cpu_perf_mask to represent the CPUs that belong to
    each cluster in a dual-cluster, heterogeneous system.
    
    Note that this assumes that CONFIG_NR_CPUS is set to exatly the number of
    CPUs present, and that the number of CPUs per cluster is split evenly in
    half.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: msdx321 <msdx321@gmail.com>
    [yaro]: adjusted for sd6150
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Udit Karode <udit.karode@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -97,6 +97,8 @@ extern struct cpumask __cpu_isolated_mask;
 #define cpu_present_mask  ((const struct cpumask *)&__cpu_present_mask)
 #define cpu_active_mask   ((const struct cpumask *)&__cpu_active_mask)
 #define cpu_isolated_mask ((const struct cpumask *)&__cpu_isolated_mask)
+extern const struct cpumask *const cpu_lp_mask;
+extern const struct cpumask *const cpu_perf_mask;
 
 #if NR_CPUS > 1
 #define num_online_cpus()	cpumask_weight(cpu_online_mask)
