99119cca4dc5aee6fed1a424a68fe4f3b4b020fc
Wei Wang <wvw@google.com>
2019-05-24 19:18:36 -0700

    Revert "sched: Improve the scheduler"
    This reverts commit a3dd94a1bb80ec98924070f28ba80d93a4d559a6.
    
    Bug:133481659
    Test: build
    Change-Id: Ib23609315f3446223521612621fe54469537c172
    Signed-off-by: Wei Wang <wvw@google.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/cpufreq/freq_table.c
+++ drivers/cpufreq/freq_table.c
@@ -73,9 +73,6 @@ int cpufreq_frequency_table_cpuinfo(struct cpufreq_policy *policy,
 			policy->min = CONFIG_MIN_FREQ_PERF;
 	#endif
 
-	if (max_freq > cpuinfo_max_freq_cached)
-		cpuinfo_max_freq_cached = max_freq;
-
 	if (policy->min == ~0)
 		return -EINVAL;
 	else
