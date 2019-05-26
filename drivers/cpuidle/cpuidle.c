841235bd665a8e382db95515c9ac6b053fea1620
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-26 10:18:46 -0700

    cpuidle: Optimize pm_qos notifier callback
    The pm_qos notifier currently suffers from a number of pitfalls: it
    sends IPIs to CPUs that may not be idle, waits for the IPIs to finish
    propagating while preemption is disabled (resulting in a long busy wait
    for the pm_qos_update_target caller), needlessly calls a no-op
    function when the IPIs are processed, and sends IPIs even when the CPU
    latency requirement is relaxed.
    
    Optimize the pm_qos notifier by using arch_send_call_function_ipi_mask
    instead of smp_call_function_many to eliminate the need to call a no-op
    function (with the added benefit of it not blocking) and only sending
    IPIs to CPUs that are idle. Additionally, only send IPIs when the CPU
    latency requirement is tightened, since it's not necessary to kick CPUs
    out of idle when the latency requirement is relaxed.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [yaro]: Resolved conflicts, adapted for updated driver.
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -37,6 +37,9 @@ static int enabled_devices;
 static int off __read_mostly;
 static int initialized __read_mostly;
 
+static void cpuidle_set_idle_cpu(unsigned int cpu);
+static void cpuidle_clear_idle_cpu(unsigned int cpu);
+
 int cpuidle_disabled(void)
 {
 	return off;
@@ -219,7 +222,9 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 	time_start = ns_to_ktime(local_clock());
 
 	stop_critical_timings();
+	cpuidle_set_idle_cpu(dev->cpu);
 	entered_state = target_state->enter(dev, drv, index);
+	cpuidle_clear_idle_cpu(dev->cpu);
 	start_critical_timings();
 
 	sched_clock_idle_wakeup_event();
@@ -640,25 +645,20 @@ int cpuidle_register(struct cpuidle_driver *drv,
 EXPORT_SYMBOL_GPL(cpuidle_register);
 
 #ifdef CONFIG_SMP
+static atomic_t idle_cpu_mask = ATOMIC_INIT(0);
 
-static void wake_up_idle_cpus(void *v)
+#if NR_CPUS > 32
+#error idle_cpu_mask not big enough for NR_CPUS
+#endif
+
+static void cpuidle_set_idle_cpu(unsigned int cpu)
 {
-	int cpu;
-	struct cpumask cpus;
+	atomic_or(BIT(cpu), &idle_cpu_mask);
+}
 
-	preempt_disable();
-	if (v) {
-		cpumask_andnot(&cpus, v, cpu_isolated_mask);
-		cpumask_and(&cpus, &cpus, cpu_online_mask);
-	} else
-		cpumask_andnot(&cpus, cpu_online_mask, cpu_isolated_mask);
-
-	for_each_cpu(cpu, &cpus) {
-		if (cpu == smp_processor_id())
-			continue;
-		wake_up_if_idle(cpu);
-	}
-	preempt_enable();
+static void cpuidle_clear_idle_cpu(unsigned int cpu)
+{
+	atomic_andnot(BIT(cpu), &idle_cpu_mask);
 }
 
 /*
@@ -670,7 +670,28 @@ static void wake_up_idle_cpus(void *v)
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	wake_up_idle_cpus(v);
+	static unsigned long prev_latency[NR_CPUS] = {
+		[0 ... NR_CPUS - 1] = PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE
+	};
+	struct cpumask update_mask = CPU_MASK_NONE;
+	unsigned int cpu;
+
+	/* Only send an IPI when the CPU latency requirement is tightened */
+	for_each_cpu(cpu, v) {
+		if (l < prev_latency[cpu])
+			cpumask_set_cpu(cpu, &update_mask);
+		prev_latency[cpu] = l;
+	}
+
+	if (!cpumask_empty(&update_mask)) {
+		unsigned long idle_cpus = atomic_read(&idle_cpu_mask);
+		cpumask_and(&update_mask, &update_mask, to_cpumask(&idle_cpus));
+		cpumask_andnot(&update_mask, &update_mask, cpu_isolated_mask);
+
+		/* Notifier is called with preemption disabled */
+		arch_send_call_function_ipi_mask(&update_mask);
+	}
+
 	return NOTIFY_OK;
 }
 
@@ -684,6 +705,14 @@ static inline void latency_notifier_init(struct notifier_block *n)
 }
 
 #else /* CONFIG_SMP */
+
+static void cpuidle_set_idle_cpu(unsigned int cpu)
+{
+}
+
+static void cpuidle_clear_idle_cpu(unsigned int cpu)
+{
+}
 
 #define latency_notifier_init(x) do { } while (0)
 
