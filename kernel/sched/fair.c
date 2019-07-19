8df327fcb1863b34e023c56a120286d7e799286a
Yaroslav Furman <yaro330@gmail.com>
2019-07-19 15:26:40 +0300

    sched/fair: only enable WALT task rotation for little cpus
    We only have 2 big CPUs, shuffling tasks between them
    is useless and causes spikes in uibench render jitter test.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12869,7 +12869,8 @@ void walt_rotate_work_init(void)
 	for_each_possible_cpu(i) {
 		struct walt_rotate_work *wr = &per_cpu(walt_rotate_works, i);
 
-		INIT_WORK(&wr->w, walt_rotate_work_func);
+		if (cpumask_test_cpu(i, cpu_lp_mask))
+			INIT_WORK(&wr->w, walt_rotate_work_func);
 	}
 }
 
