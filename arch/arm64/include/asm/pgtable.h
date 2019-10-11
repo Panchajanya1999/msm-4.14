38c02f14665453a6364bb9d6ec77ee3ffe87de62
Jia He <justin.he@arm.com>
2019-10-11 22:09:37 +0800

    arm64: mm: implement arch_faults_on_old_pte() on arm64
    On arm64 without hardware Access Flag, copying from user will fail because
    the pte is old and cannot be marked young. So we always end up with zeroed
    page after fork() + CoW for pfn mappings. We don't always have a
    hardware-managed Access Flag on arm64.
    
    Hence implement arch_faults_on_old_pte on arm64 to indicate that it might
    cause page fault when accessing old pte.
    
    Signed-off-by: Jia He <justin.he@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Alex Winkowski <dereference23@outlook.com>

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -813,6 +813,20 @@ static inline void update_mmu_cache(struct vm_area_struct *vma,
 #define kc_vaddr_to_offset(v)	((v) & ~VA_START)
 #define kc_offset_to_vaddr(o)	((o) | VA_START)
 
+/*
+ * On arm64 without hardware Access Flag, copying from user will fail because
+ * the pte is old and cannot be marked young. So we always end up with zeroed
+ * page after fork() + CoW for pfn mappings. We don't always have a
+ * hardware-managed access flag on arm64.
+ */
+static inline bool arch_faults_on_old_pte(void)
+{
+	WARN_ON(preemptible());
+
+	return !cpu_has_hw_af();
+}
+#define arch_faults_on_old_pte arch_faults_on_old_pte
+
 #endif /* !__ASSEMBLY__ */
 
 #endif /* __ASM_PGTABLE_H */
