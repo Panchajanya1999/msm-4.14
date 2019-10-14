dfbb1a1d411d333b3a27834cee33721cea952665
Zachariah Kennedy <zkennedy87@gmail.com>
2019-10-14 14:46:06 +0000

    sched/fair: if sync flag ignored, try to place in same cluster
    If the sync flag is ignored because the current cpu is not in
    the affinity mask for the target of a sync wakeup (usually
    binder call), prefer to place in the mid cluster if
    possible. The main case is a "top-app" task waking a
    "foreground" task when the top-app task is running on
    a CPU that is not in the foreground cpuset. This patch
    causes the search order from mid capacity cpu be used
    when the sync flag failed.
    
    Based on: https://github.com/aosp-mirror/kernel_msm/commit/580cecaa6bf324565694ddc688525ccd934d298f#diff-99e5389415cdd1d6dbf020da863c1ef5
    
    Made into a sched feature and added  tracepoints for testing
    
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Change-Id: I560944de345756316697c14f8dc2354480e2aba3
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7410,12 +7410,12 @@ static bool is_packing_eligible(struct task_struct *p, int target_cpu,
 }
 
 static int start_cpu(struct task_struct *p, bool boosted,
-		     struct cpumask *rtg_target)
+		     bool sync_boost, struct cpumask *rtg_target)
 {
 	struct root_domain *rd = cpu_rq(smp_processor_id())->rd;
 	int start_cpu = -1;
 
-	if (boosted) {
+	if (boosted || sync_boost) {
 		if (rd->mid_cap_orig_cpu != -1 &&
 		    task_fits_max(p, rd->mid_cap_orig_cpu))
 			return rd->mid_cap_orig_cpu;
@@ -7451,7 +7451,7 @@ enum fastpaths {
 };
 
 static inline int find_best_target(struct task_struct *p, int *backup_cpu,
-				   bool boosted, bool prefer_idle,
+				   bool boosted, bool sync_boost, bool prefer_idle,
 				   struct find_best_target_env *fbt_env)
 {
 	unsigned long min_util = boosted_task_util(p);
@@ -7494,7 +7494,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		target_capacity = 0;
 
 	/* Find start CPU based on boost value */
-	cpu = start_cpu(p, boosted, fbt_env->rtg_target);
+	cpu = start_cpu(p, boosted, sync_boost, fbt_env->rtg_target);
 	if (cpu < 0)
 		return -1;
 
@@ -8160,6 +8160,7 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 				     int sync)
 {
 	int use_fbt = sched_feat(FIND_BEST_TARGET);
+	int use_sync_boost = sched_feat(SYNC_BOOST);
 	int cpu_iter, eas_cpu_idx = EAS_CPU_NXT;
 	int delta = 0;
 	int target_cpu = -1;
@@ -8171,6 +8172,7 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 	u64 start_t = 0;
 	int next_cpu = -1, backup_cpu = -1;
 	int boosted = (schedtune_task_boost(p) > 0 || per_task_boost(p) > 0);
+	bool sync_boost = false;
 
 	fbt_env.fastpath = 0;
 
@@ -8185,6 +8187,13 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 		target_cpu = cpu;
 		fbt_env.fastpath = SYNC_WAKEUP;
 		goto out;
+	}
+
+
+	if (use_sync_boost) {
+		sync_boost = sync && cpu >= cpu_rq(cpu)->rd->mid_cap_orig_cpu;
+	} else {
+		sync_boost = false;
 	}
 
 	/* prepopulate energy diff environment */
@@ -8243,7 +8252,7 @@ static int find_energy_efficient_cpu(struct sched_domain *sd,
 
 		/* Find a cpu with sufficient capacity */
 		target_cpu = find_best_target(p, &eenv->cpu[EAS_CPU_BKP].cpu_id,
-					      boosted, prefer_idle, &fbt_env);
+					      boosted, sync_boost, prefer_idle, &fbt_env);
 		if (target_cpu < 0)
 			goto out;
 
