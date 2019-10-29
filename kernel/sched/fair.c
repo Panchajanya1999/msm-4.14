8e33d40fe61cc93f911d6dae4c1e5fc71563a4cf
Panchajanya1999 <panchajanya@azure-dev.live>
2019-10-29 20:01:47 +0530

    sched/fair: Fix wake_affine_idle() f38a7fd24746286692f88e47101bce8d061154b0 did a fuckup. Fix it..!!
    note: Pie source only
    
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8390,7 +8390,7 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 
 		if (sysctl_sched_sync_hint_enable && sync &&
 				_cpus_allowed && !_wake_cap &&
-				wake_affine_idle(cpu, prev_cpu, sync) &&
+				wake_affine_idle(sd, p, cpu, prev_cpu, sync) &&
 				cpu_is_in_target_set(p, cpu)) {
 			rcu_read_unlock();
 			return cpu;
