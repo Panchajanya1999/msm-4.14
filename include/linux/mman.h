f6e7b1c8d47894fdd49b603ba538d6062f36a86e
Catalin Marinas <catalin.marinas@arm.com>
2019-11-25 17:27:06 +0000

    mm: Introduce arch_validate_flags()
    Similarly to arch_validate_prot() called from do_mprotect_pkey(), an
    architecture may need to sanity-check the new vm_flags.
    
    Define a dummy function always returning true. In addition to
    do_mprotect_pkey(), also invoke it from mmap_region() prior to updating
    vma->vm_page_prot to allow the architecture code to veto potentially
    inconsistent vm_flags.
    
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Alex Winkowski <dereference23@outlook.com>

--- include/linux/mman.h
+++ include/linux/mman.h
@@ -57,6 +57,19 @@ static inline bool arch_validate_prot(unsigned long prot)
 #define arch_validate_prot arch_validate_prot
 #endif
 
+#ifndef arch_validate_flags
+/*
+ * This is called from mmap() and mprotect() with the updated vma->vm_flags.
+ *
+ * Returns true if the VM_* flags are valid.
+ */
+static inline bool arch_validate_flags(unsigned long flags)
+{
+	return true;
+}
+#define arch_validate_flags arch_validate_flags
+#endif
+
 /*
  * Optimisation macro.  It is equivalent to:
  *      (x & bit1) ? bit2 : 0
