ecf2cab96c884b5bc257eacaf0b50aa8a7230aca
Panchajanya1999 <panchajanya@azure-dev.live>
2020-01-01 22:46:00 +0530

    trace: Disable DEBUG_FS dependencies
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -7922,6 +7922,7 @@ init_tracer_tracefs(struct trace_array *tr, struct dentry *d_tracer)
 	ftrace_init_tracefs(tr, d_tracer);
 }
 
+#ifdef CONFIG_DEBUG_FS
 static struct vfsmount *trace_automount(struct dentry *mntpt, void *ingore)
 {
 	struct vfsmount *mnt;
@@ -7943,6 +7944,7 @@ static struct vfsmount *trace_automount(struct dentry *mntpt, void *ingore)
 
 	return mnt;
 }
+#endif
 
 /**
  * tracing_init_dentry - initialize top level trace array
@@ -7970,12 +7972,14 @@ struct dentry *tracing_init_dentry(void)
 	 * the tracefs file system there, so older tools still
 	 * work with the newer kerenl.
 	 */
+#ifdef CONFIG_DEBUG_FS
 	tr->dir = debugfs_create_automount("tracing", NULL,
 					   trace_automount, NULL);
 	if (!tr->dir) {
 		pr_warn_once("Could not create debugfs directory 'tracing'\n");
 		return ERR_PTR(-ENOMEM);
 	}
+#endif
 
 	return NULL;
 }
