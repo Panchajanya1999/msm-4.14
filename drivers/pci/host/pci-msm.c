82e6bb788fc02508cf0ecfaf61bd6287d129a7cc
Panchajanya1999 <panchajanya@azure-dev.live>
2020-02-16 14:54:44 +0530

    drivers: pci-msm: Treat IPA context error messages as debug messages
    We already disabled IPC logging, so treat alongwise.
    
    Change-Id: Ifefb9c3988fe9bcdcdf785f5ef80f07abda1a3cb
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/pci/host/pci-msm.c
+++ drivers/pci/host/pci-msm.c
@@ -6794,7 +6794,7 @@ static int __init pcie_init(void)
 		msm_pcie_dev[i].ipc_log =
 			ipc_log_context_create(PCIE_LOG_PAGES, rc_name, 0);
 		if (msm_pcie_dev[i].ipc_log == NULL)
-			pr_err("%s: unable to create IPC log context for %s\n",
+			pr_debug("%s: unable to create IPC log context for %s\n",
 				__func__, rc_name);
 		else
 			PCIE_DBG(&msm_pcie_dev[i],
@@ -6804,7 +6804,7 @@ static int __init pcie_init(void)
 		msm_pcie_dev[i].ipc_log_long =
 			ipc_log_context_create(PCIE_LOG_PAGES, rc_name, 0);
 		if (msm_pcie_dev[i].ipc_log_long == NULL)
-			pr_err("%s: unable to create IPC log context for %s\n",
+			pr_debug("%s: unable to create IPC log context for %s\n",
 				__func__, rc_name);
 		else
 			PCIE_DBG(&msm_pcie_dev[i],
@@ -6814,7 +6814,7 @@ static int __init pcie_init(void)
 		msm_pcie_dev[i].ipc_log_dump =
 			ipc_log_context_create(PCIE_LOG_PAGES, rc_name, 0);
 		if (msm_pcie_dev[i].ipc_log_dump == NULL)
-			pr_err("%s: unable to create IPC log context for %s\n",
+			pr_debug("%s: unable to create IPC log context for %s\n",
 				__func__, rc_name);
 		else
 			PCIE_DBG(&msm_pcie_dev[i],
