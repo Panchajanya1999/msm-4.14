876e1f7c8717ea5daf6a741fd9c83aff80384946
Panchajanya1999 <panchajanya@azure-dev.live>
2020-03-20 21:09:45 +0530

    nt36xxx: Bind touchscreen kthreads on little clusters
    As per logic, display running on BIG clusters is a massive waste
    of energy. It is a better idea to force ts kthreads on core 1-5, thus
    improving power consumption.
    
    Change-Id: Ifc84dc7c67a794b7f7ead724496d44639dd8a09b
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/input/touchscreen/nvt_touch_f7b/nt36xxx.c
+++ drivers/input/touchscreen/nvt_touch_f7b/nt36xxx.c
@@ -27,6 +27,7 @@
 #include <linux/input/mt.h>
 #include <linux/of_gpio.h>
 #include <linux/of_irq.h>
+#include <linux/cpumask.h>
 
 #if defined(CONFIG_FB)
 #include <linux/notifier.h>
@@ -1202,6 +1203,7 @@ return:
 static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
 	int32_t ret = 0;
+	int i;
 #if ((TOUCH_KEY_NUM > 0) || WAKEUP_GESTURE)
 	int32_t retry = 0;
 #endif
@@ -1255,7 +1257,7 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 
 	//---create workqueue---
 	kthread_init_worker(&nvt_worker);
-	nvt_worker_thread = kthread_run(kthread_worker_fn, 
+	nvt_worker_thread = kthread_create(kthread_worker_fn, 
 					&nvt_worker, "nvt_worker_thread");
 	if(IS_ERR(nvt_worker_thread)) {
 		NVT_ERR("nvt_wq create workqueue failed\n");
@@ -1263,6 +1265,21 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 		goto err_create_nvt_wq_failed;
 	}
 	sched_setscheduler(nvt_worker_thread, SCHED_FIFO, &param);
+
+	cpumask_t nvt_sys_mask;
+
+	cpumask_clear(&nvt_sys_mask);
+
+	/* Hardcode the cpumask and bind the display kthreads to little cores [ 1 - 5 ] */
+	for (i = 1; i <= 5; i++) {
+		cpumask_set_cpu(i, &nvt_sys_mask);
+	}
+
+	/* Bind workers to cpumasks */
+	kthread_bind_mask(nvt_worker_thread, &nvt_sys_mask);
+
+	/* Wake up the process on probing */
+	wake_up_process(nvt_worker_thread);
 	
 	kthread_init_work(&ts->nvt_work, &nvt_ts_work_func);
 
@@ -1283,7 +1300,6 @@ static int32_t nvt_ts_probe(struct i2c_client *client, const struct i2c_device_i
 #endif
 
 	ts->int_trigger_type = INT_TRIGGER_TYPE;
-
 
 	//---set input device info.---
 	ts->input_dev->evbit[0] = BIT_MASK(EV_SYN) | BIT_MASK(EV_KEY) | BIT_MASK(EV_ABS) ;
