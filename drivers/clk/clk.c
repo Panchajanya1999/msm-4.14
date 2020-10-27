18e72e998ce83d8aab6255b871c361bb9a0148f2
Panchajanya1999 <panchajanya@azure-dev.live>
2020-10-27 21:04:02 +0530

    drivers/clk: Guard explicit structures for DEBUG_FS
    Unused if DEBUG_FS is disabled.
    
    Change-Id: Ie994661fc86c0b04abf7324e01134d3b187d9afa
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/clk/clk.c
+++ drivers/clk/clk.c
@@ -67,10 +67,12 @@ static struct hlist_head *all_lists[] = {
 	NULL,
 };
 
+#ifdef CONFIG_DEBUG_FS
 static struct hlist_head *orphan_list[] = {
 	&clk_orphan_list,
 	NULL,
 };
+#endif
 
 /***    private data structures    ***/
 
