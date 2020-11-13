60493733635093326e6e0d3a4d74ebd8c6fbb0ef
Panchajanya1999 <panchajanya@azure-dev.live>
2020-11-13 21:14:25 +0530

    Revert "include/linux: overflow: Revert commit 80459b71e2ce64b39eafc2422212e4c99066d118"
    Android has already addressed this issue with commit 4861ce9ef48a7e790c73bd554c0aee6fc3f8041d
    (ANDROID: overflow.h: fix merge issue with 4.14.196). This revert is
    now acceptable, since we should abide by linux-stable changes.
    
    This reverts commit a15f77bf1d938749839946f3cb365a0d6e9e1ae0.
    
    Change-Id: Icf627d54f03c8b1025f5cfd9ba142ca6fa5af792
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- include/linux/overflow.h
+++ include/linux/overflow.h
@@ -250,4 +250,61 @@
 		(*_d >> _to_shift) != _a);				\
 })
 
+/**
+ * array_size() - Calculate size of 2-dimensional array.
+ *
+ * @a: dimension one
+ * @b: dimension two
+ *
+ * Calculates size of 2-dimensional array: @a * @b.
+ *
+ * Returns: number of bytes needed to represent the array or SIZE_MAX on
+ * overflow.
+ */
+static inline __must_check size_t array_size(size_t a, size_t b)
+{
+	size_t bytes;
+
+	if (check_mul_overflow(a, b, &bytes))
+		return SIZE_MAX;
+
+	return bytes;
+}
+
+/**
+ * array3_size() - Calculate size of 3-dimensional array.
+ *
+ * @a: dimension one
+ * @b: dimension two
+ * @c: dimension three
+ *
+ * Calculates size of 3-dimensional array: @a * @b * @c.
+ *
+ * Returns: number of bytes needed to represent the array or SIZE_MAX on
+ * overflow.
+ */
+static inline __must_check size_t array3_size(size_t a, size_t b, size_t c)
+{
+	size_t bytes;
+
+	if (check_mul_overflow(a, b, &bytes))
+		return SIZE_MAX;
+	if (check_mul_overflow(bytes, c, &bytes))
+		return SIZE_MAX;
+
+	return bytes;
+}
+
+static inline __must_check size_t __ab_c_size(size_t n, size_t size, size_t c)
+{
+	size_t bytes;
+
+	if (check_mul_overflow(n, size, &bytes))
+		return SIZE_MAX;
+	if (check_add_overflow(bytes, c, &bytes))
+		return SIZE_MAX;
+
+	return bytes;
+}
+
 #endif /* __LINUX_OVERFLOW_H */
