d815ccc0505b38cc4ff22206a60cc673b9901368
Panchajanya1999 <panchajanya@azure-dev.live>
2021-01-15 20:28:02 +0530

    kernel/power: main: Inhibit init from tampering memory sleep states
    The default suspend mode is s2idle for Pixels / Pixel DTs. Pixel
    kernels has the required changes for s2idle to function properly
    on Pixels.
    Unfortunately, even with these changes, users experiences rare issues
    like device kicking out of sleep or random idle drains. After a brief testings,
    it is concluded that "deep" performs better than "sleep-to-idle". For this reason, the init is being blocked from making changes to
    sleep states.
    
    A brief documentation : https://www.kernel.org/doc/Documentation/power/states.txt
    
    Fixes: dt2w for non-pixel devices, device wakes quickly after deep sleep
    Suggested-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: Ic45bdaf6e7c23882a17fbe6aec08a7d7d6dfe130
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -15,6 +15,7 @@
 #include <linux/workqueue.h>
 #include <linux/debugfs.h>
 #include <linux/seq_file.h>
+#include <linux/binfmts.h>
 
 #include "power.h"
 
@@ -126,6 +127,10 @@ static ssize_t mem_sleep_store(struct kobject *kobj, struct kobj_attribute *attr
 {
 	suspend_state_t state;
 	int error;
+
+	/* Apply init protection, else values will get overwritten */
+	if (task_is_booster(current))
+		return n;
 
 	error = pm_autosleep_lock();
 	if (error)
