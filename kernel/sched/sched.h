927790830f29502c0d17eb44fecd41a54ea0841e
Patrick Bellasi <patrick.bellasi@arm.com>
2021-02-09 11:09:11 +0530

    UPSTREAM: sched/uclamp: Enforce last task's UCLAMP_MAX
    When a task sleeps it removes its max utilization clamp from its CPU.
    However, the blocked utilization on that CPU can be higher than the max
    clamp value enforced while the task was running. This allows undesired
    CPU frequency increases while a CPU is idle, for example, when another
    CPU on the same frequency domain triggers a frequency update, since
    schedutil can now see the full not clamped blocked utilization of the
    idle CPU.
    
    Fix this by using:
    
      uclamp_rq_dec_id(p, rq, UCLAMP_MAX)
        uclamp_rq_max_value(rq, UCLAMP_MAX, clamp_value)
    
    to detect when a CPU has no more RUNNABLE clamped tasks and to flag this
    condition.
    
    Don't track any minimum utilization clamps since an idle CPU never
    requires a minimum frequency. The decay of the blocked utilization is
    good enough to reduce the CPU frequency.
    
    Bug: 120440300
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Alessio Balsini <balsini@android.com>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Morten Rasmussen <morten.rasmussen@arm.com>
    Cc: Paul Turner <pjt@google.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Quentin Perret <quentin.perret@arm.com>
    Cc: Rafael J . Wysocki <rafael.j.wysocki@intel.com>
    Cc: Steve Muckle <smuckle@google.com>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Todd Kjos <tkjos@google.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Viresh Kumar <viresh.kumar@linaro.org>
    Link: https://lkml.kernel.org/r/20190621084217.8167-4-patrick.bellasi@arm.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit e496187da71070687b55ff455e7d8d7d7f0ae0b9)
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Change-Id: Ie9eab897eb654ec9d4fba5eda20f66a91a712817
    Signed-off-by: Quentin Perret <qperret@google.com>
    (cherry picked from commit 3732b993c9483d1574775b01248bcefbf827c0b7)
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -862,8 +862,9 @@ struct rq {
 #ifdef CONFIG_UCLAMP_TASK
 	/* Utilization clamp values based on CPU's RUNNABLE tasks */
 	struct uclamp_rq	uclamp[UCLAMP_CNT] ____cacheline_aligned;
+	unsigned int		uclamp_flags;
+#define UCLAMP_FLAG_IDLE 0x01
 #endif
-
 	struct cfs_rq cfs;
 	struct rt_rq rt;
 	struct dl_rq dl;
