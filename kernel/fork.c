55ba3f7b9bddde51d14813d74b45faf7a895d8b5
Panchajanya1999 <panchajanya@azure-dev.live>
2021-02-11 21:45:51 +0530

    kernel: Boost CPU frequency to max upon zygote forking
    Helps in cold app launches.
    
    Change-Id: I829010e72203057853661554d935f985d9cf5d77
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/fork.c
+++ kernel/fork.c
@@ -96,6 +96,7 @@
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2252,9 +2253,11 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	/* Boost DDR bus to the max when userspace launches an app */
-	if (task_is_zygote(current))
+	if (task_is_zygote(current)) {
+		cpu_input_boost_kick_max(5000);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW,
 			CONFIG_DEVFREQ_INPUT_BOOST_DURATION_MS);
+	}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
