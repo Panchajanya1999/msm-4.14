9c8758944a5eb3a585bd79f503dd1d37bb373916
Sultan Alsawaf <sultan@kerneltoast.com>
2021-02-11 21:40:23 +0530

    drm/atomic: Boost CPU when committing a new frame
    In order to reduce jank, request a CPU boost whenever a new
    frame is ready to be rendered to the display. The boosts should be
    sufficient enough to render 60 FPS without any dropped frames when there
    is no significant external source of load.
    
    Change-Id: I967a4f786c89e5ac1bb559167cfe8d345213a02a
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -31,6 +31,7 @@
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
 #include <linux/sync_file.h>
+#include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
 
 #include "drm_crtc_internal.h"
@@ -2250,6 +2251,7 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		cpu_input_boost_kick();
 
 	}
 
