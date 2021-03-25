9d332c2ae60f23e973c5b8e4e52cb511139f17c2
darkhz <kmachanwenw@gmail.com>
2021-03-25 15:17:51 +0530

    uclamp_assist: Implement Uclamp Assist
    This is an implementation of a mechanism which indulges in modifying Utilization Clamping
    configurations via kernel, thus relaxing any kind of edits in device tree or ramdisk files.
    
    Signed-off-by: darkhz <kmachanwenw@gmail.com>
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    (cherry picked from commit c1be3bceaf92fcc7a3dc6a2b3a7925085f408eef)

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -1702,6 +1702,11 @@ out_unlock:
 	return retval;
 }
 
+#ifdef CONFIG_UCLAMP_ASSIST
+static void uclamp_set(struct kernfs_open_file *of,
+		size_t nbytes, loff_t off);
+#endif
+
 /*
  * Common handling for a write to a "cpus" or "mems" file.
  */
@@ -1760,6 +1765,10 @@ static ssize_t cpuset_write_resmask(struct kernfs_open_file *of,
 	}
 
 	free_trial_cpuset(trialcs);
+#ifdef CONFIG_UCLAMP_ASSIST
+	uclamp_set(of, nbytes, off);
+#endif
+
 out_unlock:
 	mutex_unlock(&cpuset_mutex);
 	kernfs_unbreak_active_protection(of->kn);
@@ -2055,6 +2064,50 @@ static struct cftype files[] = {
 #endif
 	{ }	/* terminate */
 };
+
+#ifdef CONFIG_UCLAMP_ASSIST
+struct ucl_param {
+	char *name;
+	char uclamp_min[3];
+	char uclamp_max[3];
+	u64  uclamp_latency_sensitive;
+	u64  uclamp_boosted;
+};
+
+static void uclamp_set(struct kernfs_open_file *of,
+		size_t nbytes, loff_t off)
+{
+	int i;
+
+	struct cpuset *cs = css_cs(of_css(of));
+
+	const char *cs_name = cs->css.cgroup->kn->name;
+
+	static struct ucl_param tgts[] = {
+		{"top-app",    	     	"10", "100", 1, 1},
+		{"foreground", 	     	"0",  "50",  1, 1},
+		{"background", 	     	"20", "100", 0, 0},
+		{"system-background", 	"0",  "40",  0, 0},
+	};
+
+	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
+		struct ucl_param tgt = tgts[i];
+
+		if (!strncmp(cs_name, tgt.name, strlen(tgt.name))) {
+			cpu_uclamp_min_write_wrapper(of, tgt.uclamp_min,
+				nbytes, off);
+			cpu_uclamp_max_write_wrapper(of, tgt.uclamp_max,
+				nbytes, off);
+			cpu_uclamp_ls_write_u64_wrapper(&cs->css, NULL,
+				tgt.uclamp_latency_sensitive);
+			cpu_uclamp_boost_write_u64_wrapper(&cs->css, NULL,
+				tgt.uclamp_boosted);
+
+			break;
+		}
+	}
+}
+#endif
 
 /*
  *	cpuset_css_alloc - allocate a cpuset css
