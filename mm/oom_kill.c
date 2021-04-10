95dd25547dd8aa221f307bd49ee8beb13fe48cb0
Panchajanya1999 <panchajanya@azure-dev.live>
2021-04-10 20:51:10 +0530

    mm: oom killer: Suppress oom killer logs on !DEBUG_KERNEL
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -704,7 +704,9 @@ void exit_oom_victim(void)
 void oom_killer_enable(void)
 {
 	oom_killer_disabled = false;
+#ifdef CONFIG_DEBUG_KERNEL
 	pr_info("OOM killer enabled.\n");
+#endif
 }
 
 /**
@@ -741,8 +743,9 @@ bool oom_killer_disable(signed long timeout)
 		oom_killer_enable();
 		return false;
 	}
+#ifdef CONFIG_DEBUG_KERNEL
 	pr_info("OOM killer disabled.\n");
-
+#endif
 	return true;
 }
 
