a51a5cb80af56f099a5c5376fbcf93049bfbbf88
Panchajanya1999 <panchajanya@azure-dev.live>
2021-04-10 20:55:58 +0530

    drivers/power: main: Suppress parent CPU sleeping warns for !DEBUG_KERNEL
    Change-Id: I729fc108bb04df523395602b45a8a445aa04735d
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -133,9 +133,11 @@ void device_pm_add(struct device *dev)
 		 dev->bus ? dev->bus->name : "No Bus", dev_name(dev));
 	device_pm_check_callbacks(dev);
 	mutex_lock(&dpm_list_mtx);
+#ifdef CONFIG_DEBUG_KERNEL
 	if (dev->parent && dev->parent->power.is_prepared)
 		dev_warn(dev, "parent %s should not be sleeping\n",
 			dev_name(dev->parent));
+#endif
 	list_add_tail(&dev->power.entry, &dpm_list);
 	dev->power.in_dpm_list = true;
 	mutex_unlock(&dpm_list_mtx);
