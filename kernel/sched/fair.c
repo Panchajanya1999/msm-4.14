3eea80960eaf14bd79892e765fc81365c2d481ea
Panchajanya1999 <panchajanya@azure-dev.live>
2021-06-22 22:36:48 +0530

    sched/fair: Address ISO C-90 -Wdeclaration-after-statement
    Fixes a declaration after statement code.
    
    ./kernel/sched/fair.c: In function 'cpu_util_without':
    ../kernel/sched/fair.c:6126:2: warning: ISO C90 forbids mixed declarations and code [-Wdeclaration-after-statement]
     6126 |  struct cfs_rq *cfs_rq;
    
    Change-Id: I4ba3c8cb963a3b6df9d91df8677fd7df9a343e2b
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6102,6 +6102,7 @@ struct energy_env {
  */
 static unsigned long cpu_util_without(int cpu, struct task_struct *p)
 {
+	struct cfs_rq *cfs_rq;
 	unsigned int util;
 
 #ifdef CONFIG_SCHED_WALT
@@ -6123,7 +6124,6 @@ static unsigned long cpu_util_without(int cpu, struct task_struct *p)
 #ifdef CONFIG_SCHED_WALT
 	util = max_t(long, cpu_util(cpu) - task_util(p), 0);
 #else
-	struct cfs_rq *cfs_rq;
 
 	cfs_rq = &cpu_rq(cpu)->cfs;
 	util = READ_ONCE(cfs_rq->avg.util_avg);
