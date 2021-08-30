6264e1738709e4a503716399f6f4c9da937075c0
Panchajanya1999 <panchajanya@azure-dev.live>
2021-08-30 17:04:32 +0530

    Revert "proc: move /proc/sysvipc creation to where it belongs"
    This reverts commit ff1505a506ddd08b298b7fd816633e42903c02d9.
    
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- ipc/util.c
+++ ipc/util.c
@@ -86,7 +86,6 @@ static int __init ipc_init(void)
 {
 	int err_sem, err_msg;
 
-	proc_mkdir("sysvipc", NULL);
 	err_sem = sem_init();
 	WARN(err_sem, "ipc: sysv sem_init failed: %d\n", err_sem);
 	err_msg = msg_init();
