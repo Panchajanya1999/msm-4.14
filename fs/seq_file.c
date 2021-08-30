d4258c3695fb6b9f20eba2053be9077fc4b23251
Panchajanya1999 <panchajanya@azure-dev.live>
2021-08-30 17:04:23 +0530

    Revert "proc: add seq_put_decimal_ull_width to speed up /proc/pid/smaps"
    This reverts commit ffe9df5bafca9c83098a60faad10ac1a0642f529.
    
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- fs/seq_file.c
+++ fs/seq_file.c
@@ -676,20 +676,15 @@ void seq_puts(struct seq_file *m, const char *s)
 }
 EXPORT_SYMBOL(seq_puts);
 
-/**
+/*
  * A helper routine for putting decimal numbers without rich format of printf().
  * only 'unsigned long long' is supported.
- * @m: seq_file identifying the buffer to which data should be written
- * @delimiter: a string which is printed before the number
- * @num: the number
- * @width: a minimum field width
- *
- * This routine will put strlen(delimiter) + number into seq_filed.
+ * This routine will put strlen(delimiter) + number into seq_file.
  * This routine is very quick when you show lots of numbers.
  * In usual cases, it will be better to use seq_printf(). It's easier to read.
  */
-void seq_put_decimal_ull_width(struct seq_file *m, const char *delimiter,
-			 unsigned long long num, unsigned int width)
+void seq_put_decimal_ull(struct seq_file *m, const char *delimiter,
+			 unsigned long long num)
 {
 	int len;
 
@@ -703,10 +698,7 @@ void seq_put_decimal_ull_width(struct seq_file *m, const char *delimiter,
 	memcpy(m->buf + m->count, delimiter, len);
 	m->count += len;
 
-	if (!width)
-		width = 1;
-
-	if (m->count + width >= m->size)
+	if (m->count + 1 >= m->size)
 		goto overflow;
 
 	if (num < 10) {
@@ -714,7 +706,7 @@ void seq_put_decimal_ull_width(struct seq_file *m, const char *delimiter,
 		return;
 	}
 
-	len = num_to_str(m->buf + m->count, m->size - m->count, num, width);
+	len = num_to_str(m->buf + m->count, m->size - m->count, num);
 	if (!len)
 		goto overflow;
 
@@ -723,12 +715,6 @@ void seq_put_decimal_ull_width(struct seq_file *m, const char *delimiter,
 
 overflow:
 	seq_set_overflow(m);
-}
-
-void seq_put_decimal_ull(struct seq_file *m, const char *delimiter,
-			 unsigned long long num)
-{
-	return seq_put_decimal_ull_width(m, delimiter, num, 0);
 }
 EXPORT_SYMBOL(seq_put_decimal_ull);
 
@@ -805,7 +791,7 @@ void seq_put_decimal_ll(struct seq_file *m, const char *delimiter, long long num
 		return;
 	}
 
-	len = num_to_str(m->buf + m->count, m->size - m->count, num, 0);
+	len = num_to_str(m->buf + m->count, m->size - m->count, num);
 	if (!len)
 		goto overflow;
 
