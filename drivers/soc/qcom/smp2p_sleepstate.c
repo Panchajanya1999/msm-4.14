5635c75a4277271e1c1fe3a10031ba35b7315be4
Panchajanya1999 <panchajanya@azure-dev.live>
2021-11-13 21:10:48 +0530

    soc: qcom: smp2p_sleepstate: Relax wakeup source on failing to register IRQ
    smp2p_sleepstate_handler() already sets a wakeup event with a 200ms
    timeout.
    But in case an IRQ(threaded) fails to register, it will instantly
    relax the wakeup source.
    Alters no behaviour.
    
    Change-Id: I5f77f70c9f2e3602e88450176fd87986ab75f6f7
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/soc/qcom/smp2p_sleepstate.c
+++ drivers/soc/qcom/smp2p_sleepstate.c
@@ -96,6 +96,7 @@ static int smp2p_sleepstate_probe(struct platform_device *pdev)
 	if (ret) {
 		dev_err(&pdev->dev, "fail to register smp2p threaded_irq=%d\n",
 									irq);
+		__pm_relax(&notify_ws);
 		goto err;
 	}
 	return 0;
