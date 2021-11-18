75cc79d0c8209523b7d2402c694d8a92855b58a1
Andrzej Perczak <linux@andrzejperczak.com>
2021-11-18 15:59:05 +0100

    irqchip: Enable wakeup irq logging and cleanup loggers
    It helps out with battery usage analysis, otherwise, the only message
    kernel spills is: 'Resume cause unknown'.
    
    Enable it by default as printing is fairly cheap.
    
    It turns out that msm_show_resume_irq_mask controls 3 similar functions.
    The one in irq-gic-v3 sometimes misses the irq name and doesn't report
    proper irq number. I have also never seen pinctrl print anything. Because
    of this allow only pm_system_irq_wakeup to depend on the flag and make
    other functions no-ops.
    
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Change-Id: Ic6f975dfe400e21fd7fb44c76cf3b189ca9ccaf8
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -449,7 +449,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 	if (base == NULL)
 		return;
 
-	if (!msm_show_resume_irq_mask)
+	if (1)
 		return;
 
 	for (i = 0; i * 32 < gic->irq_nr; i++) {
--- drivers/pinctrl/qcom/pinctrl-msm.c
+++ drivers/pinctrl/qcom/pinctrl-msm.c
@@ -2019,7 +2019,7 @@ static void msm_pinctrl_resume(void)
 	}
 #endif
 
-	if (!msm_show_resume_irq_mask)
+	if (1)
 		return;
 
 	raw_spin_lock_irqsave(&pctrl->lock, flags);
